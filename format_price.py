e4bfb427084b05b57bb0703666895b738d64f728
Pat <120336989+pszrk@users.noreply.github.com>
2023-10-06 18:45:35 -0400

    remove unnecessary lines

--- format_price.py
+++ format_price.py
@@ -2,8 +2,3 @@ def format_price(price):
     if price >= 1:
         return f"{price:.02f}"
     return f"{price:.4g}"
-
-print(f"{format_price(0.2341234)}")
-print(f"{format_price(324534253425.23142314)}")
-print(f"{format_price(2134)}")
-print(f"{format_price(0.00000000000002341234)}")
